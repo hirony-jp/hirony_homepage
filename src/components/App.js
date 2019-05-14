a0bbdd7e277943b8396095ac3cc62d082ace6131
hirony-jp <jp.hiroki48@gmail.com>
2019-05-14 22:05:21 +0900

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome Hirony.jp</h1>
+
+      <ul>
+        <li>Twitter - <a href="https://twitter.com/_hirony">https://twitter.com/_hirony</a></li>
+        <li>Github - <a href="https://github.com/hirony-jp/">https://github.com/hirony-jp/</a></li>
+        <li>Youtube - <a href="https://www.youtube.com/channel/UCMMp1g3u_vuGJh5qv2UdhKw">https://www.youtube.com/channel/UCMMp1g3u_vuGJh5qv2UdhKw</a></li>
+        <li>マシュマロ - <a href="https://marshmallow-qa.com/_hirony">https://marshmallow-qa.com/_hirony</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
