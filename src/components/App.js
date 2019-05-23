2260e3a31e8eda36845b7a59a1f6750f0a819d92
hirony-jp <jp.hiroki48@gmail.com>
2019-05-23 21:00:35 +0900

    update

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,12 @@ function App() {
     <div>
       <h1>Welcome Hirony.jp</h1>
 
+      <p>デザイナー募集</p>
+
       <ul>
-        <li>Twitter - <a href="https://twitter.com/_hirony">https://twitter.com/_hirony</a></li>
-        <li>Github - <a href="https://github.com/hirony-jp/">https://github.com/hirony-jp/</a></li>
-        <li>Youtube - <a href="https://www.youtube.com/channel/UCMMp1g3u_vuGJh5qv2UdhKw">https://www.youtube.com/channel/UCMMp1g3u_vuGJh5qv2UdhKw</a></li>
-        <li>マシュマロ - <a href="https://marshmallow-qa.com/_hirony">https://marshmallow-qa.com/_hirony</a></li>
+        <li><a href="https://twitter.com/_hirony">Twitter</a></li>
+        <li><a href="https://www.youtube.com/channel/UCMMp1g3u_vuGJh5qv2UdhKw">Youtube</a></li>
+        <li><a href="https://marshmallow-qa.com/_hirony">マシュマロ</a></li>
       </ul>
     </div>
   );
