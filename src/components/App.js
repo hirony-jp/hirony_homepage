28fa9362e3b4e0782197ac6813cfa4f3a46a71db
Hiroki Matsumoto <jp.hiroki48@gmail.com>
2020-04-05 23:42:39 +0900

    card centering

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import NameCard from './NameCard.js';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Button } from '@material-ui/core';
 
 function App() {
   return (
     <div>
-      <h1>Welcome Hirony.jp</h1>
-
-      <p>デザイナー募集</p>
 
       <NameCard />
 
-      <ul>
+      {/* <ul>
         <li><a href="https://twitter.com/_hirony">Twitter</a></li>
         <li><a href="https://www.youtube.com/channel/UCMMp1g3u_vuGJh5qv2UdhKw">Youtube</a></li>
         <li><a href="https://marshmallow-qa.com/_hirony">マシュマロ</a></li>
-      </ul>
+      </ul> */}
     </div>
   );
 }
--- src/components/NameCard.js
+++ src/components/NameCard.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Card, CardHeader, CardContent, Typography, Avatar } from '@material-ui/core';
+import { Card, CardHeader, Avatar } from '@material-ui/core';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const useStyles = makeStyles((theme) =>
     createStyles({
         large: {
-            width: theme.spacing(10),
-            height: theme.spacing(10)
+            width: theme.spacing(7),
+            height: theme.spacing(7)
+        },
+        cardStyle: {
+            margin: 'auto',
+            marginTop: 30,
+            marginBottom: 30,
+            maxWidth: 300
+        },
+        root: {
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)'
         }
     })
 );
@@ -14,27 +27,35 @@ const useStyles = makeStyles((theme) =>
 function NameCard() {
     const classes = useStyles();
     return(
-        <Card>
-            <CardHeader
-                avatar={
-                    <Avatar 
-                        aria-label="recipe"
-                        src={`${process.env.PUBLIC_URL}/hirony-girl-icon-0000.png`}
-                        className={classes.large}
-                    />
-                }
-                title="Miharu Kageyama"
-            />
-            <CardContent>
-                
-                <Typography>
-                    Name
-                </Typography>
-                <Typography>
-                    contents
-                </Typography>
-            </CardContent>
-        </Card>
+        <div className={classes.root} >
+            <Card className={classes.cardStyle}>
+                <CardHeader
+                    titleTypographyProps={{variant: 'h5'}}
+                    title="Miharu Kageyama"
+                    avatar={
+                        <Avatar 
+                            aria-label="recipe"
+                            src={`${process.env.PUBLIC_URL}/hirony-girl-icon-0000.png`}
+                            className={classes.large}
+                        />
+                    }
+                />
+            </Card>
+
+            <Card className={classes.cardStyle}>
+                <CardHeader
+                    titleTypographyProps={{variant: 'h5'}}
+                    title="Hiroki Matsumoto"
+                    avatar={
+                        <Avatar
+                            aria-label="recipe"
+                            src={`${process.env.PUBLIC_URL}/icon-twitter-maru-maru.png`}
+                            className={classes.large}
+                        />
+                    }
+                />
+            </Card>
+        </div>
     );
 }
 
